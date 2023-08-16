848c31381d38be3ca80135c846501d40018b7510
Deepanshu <deepanshuy00730@gmail.com>
2023-08-16 23:54:58 +0530

    / end point added

--- index.js
+++ index.js
@@ -10,6 +10,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/",(req,res) => {
+    res.json({
+        message : "Server is working"
+    })
+})
 app.use("/api/auth", userRoutes);
 app.use("/api/messages", messageRoute);
 
